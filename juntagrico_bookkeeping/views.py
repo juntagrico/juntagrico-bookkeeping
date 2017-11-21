9cf2428dbe6d79a7cf8b9a3ae797575551281d2a
samuel-iseli <samuel.iseli@posteo.ch>
2017-11-21 16:17:35 +0100

    generalized excel export to replace juntagrico.utils.xls.

--- juntagrico_bookkeeping/views.py
+++ juntagrico_bookkeeping/views.py
@@ -4,10 +4,9 @@ from django import forms
 from django.contrib.auth.decorators import permission_required
 from juntagrico_bookkeeping.dao.subscriptiondao import SubscriptionDao
 from juntagrico.util.temporal import start_of_business_year, start_of_next_business_year
+from juntagrico_bookkeeping.xls import generate_excel
 import datetime 
-from io import BytesIO
-import xlsxwriter
-from django.http import HttpResponse
+import types
 
 @permission_required('juntagrico.is_operations_group')
 def subscriptions(request):
@@ -41,13 +40,13 @@ def subscriptions(request):
                         'primary_member': s.primary_member_nullsave(),
                         'size': s.size,
                         'size_name': s.size_name,
-                        'member_account': 'm_acct',
-                        'account': 'acct',
-                        'price': s.price,
+                        'member_account': 'm_account',
+                        'account': 's_account',
+                        'price': float(s.price),
                         'amount_billed': s.amount_billed } for s in entities]    
 
     if request.GET.get('format', '') == "xlsx":
-        return generate_excel(fromdate, tilldate, subscriptions)
+        return excel_export(subscriptions)
     else:
         renderdict = {
             'daterange_form': daterange_form,
@@ -55,45 +54,18 @@ def subscriptions(request):
         }
         return render(request, "bk/subscriptions.html", renderdict)
 
-
-def generate_excel(fromdate, tilldate, subscriptions):
-    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    response['Content-Disposition'] = 'attachment; filename=Abos.xlsx'
-    output = BytesIO()
-    workbook = xlsxwriter.Workbook(output)
-    worksheet = workbook.add_worksheet()
-
+def excel_export(subscriptions):
     fields = [
-        ("Start", "activation_date" ),
-        ("Ende", "deactivation_date"),
-        ("AbonnentIn", "primary_member"),
-        ("Anzahl", "size"),
-        ("Bezeichnung", "size_name"),
-        ("Konto AbonnentIn", "member_account"),
-        ("Konto Abo", "account"),
-        ("Beitrag", "price"),
-        ("Rechnung", "amount_billed")
+        ("activation_date", "Start"),
+        ("deactivation_date", "Ende"),
+        ("primary_member", "AbonnentIn"),
+        ("size", "Anzahl"),
+        ("size_name", "Bezeichnung"),
+        ("member_account", "Konto AbonnentIn"),
+        ("account", "Konto Abo"),
+        ("price", "Beitrag"),
+        ("amount_billed", "Rechnung")
     ]
 
-    # title
-    
+    return generate_excel(fields, subscriptions, "Abos")
 
-    # header row
-    col = 0
-    row = 0
-    for label, fieldname in fields:
-        worksheet.write_string(row, col, label)
-        col += 1
-
-    # data rows
-    row = 2
-    for subscription in subscriptions:
-        col = 0
-        for label, fieldname in fields:
-            worksheet.write_string(row, col, str(subscription.get(fieldname, "")))
-            col += 1
-        row += 1
-
-    workbook.close()
-    response.write(output.getvalue())
-    return response
