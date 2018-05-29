568172a56ee0387addb360eaf32ae34fc8e9110a
samuel-iseli <samuel.iseli@posteo.ch>
2018-05-29 14:57:05 +0200

    added singleton settings model, added ExtraSubscriptionCategoryAccount.

--- juntagrico_bookkeeping/entity/account.py
+++ juntagrico_bookkeeping/entity/account.py
@@ -3,6 +3,10 @@ from django.db import models
 from juntagrico.models import *
 
 class MemberAccount(models.Model):
+    """
+    Account number for members.
+    Implemented as inline admin record on member.
+    """
 
     member = models.OneToOneField(Member, on_delete=models.CASCADE, related_name='member_account')
     account = models.CharField('Konto', max_length=100)
@@ -11,9 +15,18 @@ class MemberAccount(models.Model):
         return self.member.get_name()
 
 class SubscriptionTypeAccount(models.Model):
-
+    """
+    Account number for type of subscription.
+    Implemented as inline admin record on SubscriptionType.
+    """
     subscriptiontype = models.OneToOneField(SubscriptionType, on_delete=models.CASCADE, related_name='subscriptiontype_account')
     account = models.CharField('Konto', max_length=100)
     
-    def __str__(self):
-        return self.subscriptiontype.primary_member.get_name()
+
+class ExtraSubscriptionCategoryAccount(models.Model):
+    """
+    Account number for extra subscriptions. Is located on extra subscription category.
+    Implemented as inline admin record on ExtraSubscriptionCategory.
+    """
+    extrasubcategory = models.OneToOneField(ExtraSubscriptionCategory, on_delete=models.CASCADE, related_name="extrasub_account")
+    account = models.CharField('Konto', max_length=100)
--- juntagrico_bookkeeping/models.py
+++ juntagrico_bookkeeping/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
 from juntagrico_bookkeeping.entity.account import *
+from juntagrico_bookkeeping.entity.settings import *
 
