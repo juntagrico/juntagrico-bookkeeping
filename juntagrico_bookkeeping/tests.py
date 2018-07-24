78aeb31e96a1ee7d36d8a695f37262228744f9fb
samuel-iseli <samuel.iseli@posteo.ch>
2018-07-24 16:51:58 +0200

    moved tests for scale_subscription_price to juntagrico

--- juntagrico_bookkeeping/tests.py
+++ juntagrico_bookkeeping/tests.py
@@ -100,30 +100,6 @@ class SubscriptionTestBase(django.test.TestCase):
             )
 
 
-class ScaleSuscriptionPriceTest(SubscriptionTestBase):
-
-    def test_price_by_date_fullyear(self):
-        start_date = date(2018, 1, 1)
-        end_date = date(2018, 12, 31)
-        price_fullyear = scale_subscription_price(self.subs, start_date, end_date)
-        self.assertEqual(1200.0, price_fullyear, "full year")
-
-    def test_price_by_date_quarter(self):
-        start_date = date(2018, 4, 1)
-        end_date = date(2018, 6, 30)
-        price_quarter = scale_subscription_price(self.subs, start_date, end_date)
-        price_quarter_expected = 1200.0 * (30 + 31 + 30) / 365
-        self.assertEqual(price_quarter_expected, price_quarter, "second quarter")
-
-    def test_price_by_date_partial_subscription(self):
-        self.subs.activation_date = date(2018, 7, 1)
-        self.subs.deactivation_date = date(2018, 9, 30)
-        start_date = date(2018, 1, 1)
-        end_date = date(2018, 12, 31)
-        price = scale_subscription_price(self.subs, start_date, end_date)
-        price_expected = 1200.0 * (31 + 31 + 30) / 365
-        self.assertEqual(price_expected, price, "quarter subscription over a year")
-
 class SubscriptionBookingsTest(SubscriptionTestBase):
     def test_subscription_booking_full_year(self):
         start_date = date(2018, 1, 1)
