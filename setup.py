8ed1e228aae08b5710f9e39c26709abac10cad42
motu55 <david.simmen@bluewin.ch>
2018-09-25 18:05:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='juntagrico-bookkeeping',
-    version='1.dev.0',
+    version='1.dev.1',
     packages=find_packages(),
     include_package_data=True,
     license='LPGLv3',  # example license
